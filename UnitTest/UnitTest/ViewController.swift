a2dce9b8ef2a73b5717d669f5b6ab56fd3f7ad56
오준현 <oh.junhyeon95@gmail.com>
2021-03-18 17:14:53 +0900

    유닛 테스트 공부

---
+++ UnitTest/UnitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTest
+//
+//  Created by 오준현 on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnitTest/UnitTestTests/UnitTestTests.swift
@@ -0,0 +1,32 @@
+//
+//  UnitTestTests.swift
+//  UnitTestTests
+//
+//  Created by 오준현 on 2021/03/18.
+//
+
+import XCTest
+@testable import UnitTest
+
+class UnitTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+
+        self.measure {
+
+        }
+    }
+
+}
