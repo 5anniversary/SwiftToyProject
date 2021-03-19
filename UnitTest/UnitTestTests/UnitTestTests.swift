1d6e16d06ec6af60a296707b98c61a9e9b990d88
오준현 <oh.junhyeon95@gmail.com>
2021-03-19 16:08:35 +0900

    .

--- UnitTest/UnitTestTests/UnitTestTests.swift
+++ UnitTest/UnitTestTests/UnitTestTests.swift
@@ -9,24 +9,51 @@ import XCTest
 @testable import UnitTest
 
 class UnitTestTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        
+  
+  override func setUpWithError() throws {
+    
+  }
+  
+  override func tearDownWithError() throws {
+    
+  }
+  
+  func testExample() throws {
+    
+  }
+  
+  func testPerformanceExample() throws {
+    
+    self.measure {
+      
     }
+  }
+  
+  func testVanillaLeapYear() {
+    let year = Year(calendarYear: 1996)
+    XCTAssertTrue(year.isLeapYear)
+  }
+  
+  func testAnyOldYear() {
+    let year = Year(calendarYear: 1997)
+    XCTAssertTrue(!year.isLeapYear)
+  }
+  
+  func testCentury() {
+    let year = Year(calendarYear: 1900)
+    XCTAssertTrue(!year.isLeapYear)
+  }
+  
+  func testExceptionalCentury() {
+    let year = Year(calendarYear: 2400)
+    XCTAssertTrue(year.isLeapYear)
+  }
+}
 
-    override func tearDownWithError() throws {
-
-    }
-
-    func testExample() throws {
-
-    }
-
-    func testPerformanceExample() throws {
-
-        self.measure {
-
-        }
-    }
-
+struct Year {
+  let calendarYear: Int
+  var isLeapYear: Bool {
+    return calendarYear%4 == 0 &&
+      (calendarYear%100 != 0 || calendarYear%400 == 0)
+  }
 }
